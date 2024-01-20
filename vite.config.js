31dd805412808129dace3e186dcfec1452c1ab3d
Ildar <ildar.1795@gmail.com>
2024-01-20 13:39:52 +0400

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
@@ -10,5 +10,8 @@ export default defineConfig({
 				additionalData: `@import '@/assets/styles/global.scss';`
 			}
 		}
+	},
+	define: {
+		'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
 	}
 })
