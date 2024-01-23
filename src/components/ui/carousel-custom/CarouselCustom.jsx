0bbabcd77dbd4adf70a2a85073cb0636cc625b51
Ildar <ildar.1795@gmail.com>
2024-01-23 12:58:27 +0400

    change styles

--- src/components/ui/carousel-custom/CarouselCustom.jsx
+++ src/components/ui/carousel-custom/CarouselCustom.jsx
@@ -28,22 +28,22 @@ const CarouselCustom = ({ children }) => {
 					infinite: true,
 					dots: true
 				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-					initialSlide: 2
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
 			}
+			// {
+			// 	breakpoint: 600,
+			// 	settings: {
+			// 		slidesToShow: 2,
+			// 		slidesToScroll: 2,
+			// 		initialSlide: 2
+			// 	}
+			// },
+			// {
+			// 	breakpoint: 480,
+			// 	settings: {
+			// 		slidesToShow: 1,
+			// 		slidesToScroll: 1
+			// 	}
+			// }
 		]
 	}
 	return <Slider {...settings}>{children}</Slider>
